0562dac16fb41d6af5a9c8d16d8e691fc96a7197
Babel Solea <siacraft@gmail.com>
2021-02-23 13:49:54 +0300

    list completed

--- str_lines_slices.py
+++ str_lines_slices.py
@@ -17,32 +17,32 @@
 #     print(s[i], end='')
 
 # Slice - срез для работы с целыми частями строки в диапазоне
-s = 'abcdefghij'
-print(s)
-print(s[2:5])
-print(s[0:6])
-print(s[2:7])
+# s = 'abcdefghij'
+# print(s)
+# print(s[2:5])
+# print(s[0:6])
+# print(s[2:7])
 # Slice from digit to end or from start to digit
-print(s[2:])
-print(s[:2])
-print(s[:])
+# print(s[2:])
+# print(s[:2])
+# print(s[:])
 
 # Slice with '-digits'
-print(s[-9:-4])
-print(s[-3:])
-print(s[:-3])
+# print(s[-9:-4])
+# print(s[-3:])
+# print(s[:-3])
 
 # Slice step - 3d not mandatory param in Slice what mean slice digit every X
-print()
-print(s)
-print(s[::-1]) # turn last to start digit
-print(s[::-2])
-print(s[1:7:2])
-print(s[3::2])
-print(s[:7:3])
-print(s[::2])
+# print()
+# print(s)
+# print(s[::-1]) # turn last to start digit
+# print(s[::-2])
+# print(s[1:7:2])
+# print(s[3::2])
+# print(s[:7:3])
+# print(s[::2])
 
 # Changing string digits
 # if i need change digit in string i do this code
-s = s[:4] + 'XXX' + s[5:]
-print(s)
+# s = s[:4] + 'XXX' + s[5:]
+# print(s)
